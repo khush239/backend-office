b12558e7ead47b2819f36a996e3548aa25b2b385
khush239 <khushshah9990@gmail.com>
2026-04-03 23:38:00 +0530

    Fix syntax and entrypoints for Vercel Serverless environment

--- server.js
+++ server.js
@@ -36,4 +36,8 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+if (process.env.NODE_ENV !== 'production' || process.env.LOCAL_DEV === 'true') {
+  app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+}
+
+module.exports = app;
